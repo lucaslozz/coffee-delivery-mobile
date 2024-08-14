8a50e666bc05a1a0f17d3e00dcb75a95f2a94ef9
lucasloz <lucass.mark@gmail.com>
2024-08-14 20:44:10 -0300

    feat: create Text component

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -12,18 +12,17 @@ type SRTextProps = ComponentProps<typeof SRText>;
 export interface TextProps extends SRTextProps {
   preset?: TextVariants;
   bold?: boolean;
-  semiBold?: boolean;
 }
 
 export function Text({
-  preset = 'body',
+  preset = 'paragraphMedium',
   children,
   style,
   bold,
-  semiBold,
+
   ...textProps
 }: Readonly<TextProps>) {
-  const fontFamily = getFontFamily(bold, semiBold);
+  const fontFamily = getFontFamily(bold);
 
   return (
     <SRText
@@ -35,27 +34,39 @@ export function Text({
   );
 }
 
-function getFontFamily(bold?: boolean, semiBold?: boolean) {
+function getFontFamily(bold?: boolean) {
   switch (true) {
     case bold:
       return $fontFamily.bold;
-    case semiBold:
-      return $fontFamily.semiBold;
     default:
       return $fontFamily.regular;
   }
 }
 
-export type TextVariants = 'heading' | 'subtitle' | 'body';
+export type TextVariants =
+  | 'headingExtraLarge'
+  | 'headingLarge'
+  | 'headingMedium'
+  | 'headingSmall'
+  | 'headingExtraSmall'
+  | 'paragraphLarge'
+  | 'paragraphMedium'
+  | 'paragraphSmall'
+  | 'paragraphExtraSmall';
 
 export const $fontSizes: Record<TextVariants, TextStyle> = {
-  heading: {fontSize: 22, lineHeight: 28.6},
-  subtitle: {fontSize: 16, lineHeight: 20.8},
-  body: {fontSize: 14, lineHeight: 18.2},
+  headingExtraLarge: {fontSize: 36, lineHeight: 46.8},
+  headingLarge: {fontSize: 24, lineHeight: 31.2},
+  headingMedium: {fontSize: 20, lineHeight: 26},
+  headingSmall: {fontSize: 16, lineHeight: 20.8},
+  headingExtraSmall: {fontSize: 14, lineHeight: 18.2},
+  paragraphLarge: {fontSize: 20, lineHeight: 18.2},
+  paragraphMedium: {fontSize: 16, lineHeight: 20.8},
+  paragraphSmall: {fontSize: 14, lineHeight: 18.2},
+  paragraphExtraSmall: {fontSize: 12, lineHeight: 15.6},
 };
 
 export const $fontFamily = {
-  bold: 'Sarabun_700Bold',
-  semiBold: 'Sarabun_600SemiBold',
-  regular: 'Sarabun_400Regular',
+  bold: 'Ballo2-Bold',
+  regular: 'Roboto-Regular',
 };
